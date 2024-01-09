d6a040f2d8b8bb04e4701b1c9faa7cb0d5443186
Souhaillyamani <127893978+Souhaillyamani@users.noreply.github.com>
2024-01-09 17:29:51 +0100

    test 2

--- src/test/java/edu/centralenantes/mastermind/CouleurTest.java
+++ src/test/java/edu/centralenantes/mastermind/CouleurTest.java
@@ -1,7 +1,9 @@
 package edu.centralenantes.mastermind;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  *
